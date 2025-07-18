7eae706a1daaddc2a6d2d4c01f4124a7c9b1041d
Astvit <astvit07@gmail.com>
2025-07-18 09:40:14 +0000

    moved board functions to utilities

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,24 +3,27 @@ import GridItem from "./GridItem";
 import Dictionary from "../service/dictionary";
 import {GameContext} from "./GameContext";
 
-const GRID_SIZE = 5;
+import {GRID_SIZE, emptyGrid, initBoard} from "../utils/boardUtils";
 
-function emptyGrid() {
-  return Array.from({length: GRID_SIZE}, () =>
-    Array.from({length: GRID_SIZE}, () => "")
-  );
-}
-
-function generateBoardFromWords(words) {
-  const firstWord = words.filter(word => word.length === GRID_SIZE);
-  if (firstWord.length === 0) return emptyGrid();
-
-  const word = firstWord[Math.floor(Math.random() * firstWord.length)];
-  const board = emptyGrid();
-  const centerRow = Math.floor(GRID_SIZE / 2);
-  board[centerRow] = word.split("");
-  return board;
-}
+initBoard();
+// const GRID_SIZE = 5;
+//
+// function emptyGrid() {
+//   return Array.from({length: GRID_SIZE}, () =>
+//     Array.from({length: GRID_SIZE}, () => "")
+//   );
+// }
+//
+// function generateBoardFromWords(words) {
+//   const firstWord = words.filter(word => word.length === GRID_SIZE);
+//   if (firstWord.length === 0) return emptyGrid();
+//
+//   const word = firstWord[Math.floor(Math.random() * firstWord.length)];
+//   const board = emptyGrid();
+//   const centerRow = Math.floor(GRID_SIZE / 2);
+//   board[centerRow] = word.split("");
+//   return board;
+// }
 
 export default function Grid({activePlayer}) {
   const [board, setBoard] = useState([]);
@@ -40,12 +43,20 @@ export default function Grid({activePlayer}) {
     setLetterAdded(false);
   }, [contextActivePlayer]);
 
+  // useEffect(() => {
+  //   window.Dictionary = Dictionary;
+  //
+  //   Dictionary.getDictionary().then(() => {
+  //     const words = Dictionary.getAll(); // Dictionary.dictionary
+  //     setBoard(generateBoardFromWords(words));
+  //   });
+  // }, []);
+
   useEffect(() => {
     window.Dictionary = Dictionary;
 
-    Dictionary.getDictionary().then(() => {
-      const words = Dictionary.getAll(); // Dictionary.dictionary
-      setBoard(generateBoardFromWords(words));
+    initBoard().then((newBoard) => {
+      setBoard(newBoard);
     });
   }, []);
 
